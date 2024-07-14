8f90141ba25c0cedf6fe8ebe1a257931e2f200b5
Erik Wessman <erikwessman10@gmail.com>
2024-07-14 20:07:22 +0200

    Fixed better/modular backend url environment variable

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,9 +1,8 @@
 import axios from 'axios';
-
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+import { httpUrl } from 'config';
 
 const axiosInstance = axios.create({
-  baseURL: API_URL,
+  baseURL: httpUrl,
 });
 
 axiosInstance.interceptors.request.use(
