717f3127b4be2c44a1721f7050df95ffed078b43
Erik Wessman <erikwessman10@gmail.com>
2024-07-15 18:00:50 +0200

    Prepend VITE_ to env variable

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,4 +1,4 @@
-const backendUrl = process.env.BACKEND_URL || 'localhost:5000';
+const backendUrl = process.env.VITE_BACKEND_URL || 'localhost:5000';
 
 const protocol = process.env.NODE_ENV === 'development' ? 'http://' : 'https://';
 const wsProtocol = process.env.NODE_ENV === 'development' ? 'ws://' : 'wss://';
