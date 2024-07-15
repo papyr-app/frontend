2561d4b71efdd0cfa5ade7860c2a4b540152d715
Erik Wessman <erikwessman10@gmail.com>
2024-07-15 18:09:13 +0200

    Another attempt at getting env variables to work

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,7 +1,9 @@
-const backendUrl = import.meta.env.VITE_BACKEND_URL || 'localhost:5000';
+import { env } from '$env/dynamic/private';
 
-const protocol = import.meta.env.NODE_ENV === 'development' ? 'http://' : 'https://';
-const wsProtocol = import.meta.env.NODE_ENV === 'development' ? 'ws://' : 'wss://';
+const backendUrl = env.VITE_BACKEND_URL || 'localhost:5000';
+
+const protocol = env.NODE_ENV === 'development' ? 'http://' : 'https://';
+const wsProtocol = env.NODE_ENV === 'development' ? 'ws://' : 'wss://';
 
 export const httpUrl = `${protocol}${backendUrl}`;
 export const wsUrl = `${wsProtocol}${backendUrl}`;
