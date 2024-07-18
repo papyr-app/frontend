ba2748dbca20561d2e521e0e5e956437d5560b14
Elias Carlson <carlson.elias@gmail.com>
2024-07-18 16:52:08 +0200

    Added groups for routes with and without header

---
+++ src/routes/(headerless)/+layout.svelte
@@ -0,0 +1,3 @@
+<main class="min-h-screen flex flex-col">
+	<slot />
+</main>
---
+++ src/routes/(headerless)/workspace/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import type { PageServerData } from './$types';
+	import PDFViewer from '$lib/components/PDFViewer.svelte';
+
+	export let data: PageServerData;
+	const url = data.url;
+</script>
+
+<PDFViewer {url} />
