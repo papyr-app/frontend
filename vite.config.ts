e7f8c369221ae82cb9696a55416b2925c3263a39
Erik Wessman <erikwessman10@gmail.com>
2024-07-18 17:01:05 +0200

    Updated ports and added development dockerfile

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	define: {
-		'process.env': process.env
-	}
+    plugins: [sveltekit()],
+    server: {
+        host: true,
+        port: 3000
+    },
+    preview: {
+        host: true,
+        port: 3000
+    }
 });
